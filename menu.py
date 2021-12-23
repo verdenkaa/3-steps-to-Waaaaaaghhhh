315d82906ec9f204cba2f207e5e9573f8f06e2b0
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2021-12-23 17:44:07 +0300

    Menu

---
+++ menu.py
@@ -0,0 +1,31 @@
+import pygame
+import os
+
+class Menu():
+    pass
+
+
+def draw_menu(screen):
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_TAB:
+                    running = False
+                    pygame.quit()
+                    os.system('python persons.py')             
+
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = 500, 500
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('3-step to Waagh!')
+    draw_menu(screen)
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
