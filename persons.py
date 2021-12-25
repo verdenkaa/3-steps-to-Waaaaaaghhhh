65ebd91935d604bf77acd8a1359ec68433b07c23
verdenkaa <nikitapahomov97@gmail.com>
2021-12-25 17:58:34 +0300

    AllOrcs
    All persons, stop dakka, add voises

--- persons.py
+++ persons.py
@@ -20,7 +20,7 @@ def intround(x, left, right):
 
 class Player():
     
-    def __init__(self, patron, person, Bx, By, Lx, Ly, ammo, maxammo, plusammo=0.5):
+    def __init__(self, patron, person, Bx, By, Lx, Ly, ammo, maxammo, legsmove=100):
         #pygame.sprite.Sprite.__init__(self)
         self.shooting = False
         self.go = False
@@ -30,7 +30,7 @@ class Player():
         self.rect.center = self.position
         self.image2 = load_image(f"/{person}/Legs.png", Lx, Ly)
         self.rect2 = self.image.get_rect()
-        self.rect2.center = self.position[0] + 5, self.position[1] + 100
+        self.rect2.center = self.position[0] + 5, self.position[1] + legsmove
         self.rot_image = self.image
         self.mask = pygame.mask.from_surface(self.image)
         self.cadr = 0
@@ -42,7 +42,11 @@ class Player():
         self.f = pygame.font.Font(None, 40)
         self.patron = patron
         self.soundShoot1 = pg.mixer.Sound('Sounds/boltshoot.mp3')
-        self.plusammo = plusammo
+        self.plusammo = 0.5
+        self.cadrtoshoot = 5
+        self.todakka = 0
+        self.t = True
+        self.timer = 200
         
     def load_image(name, h, w, colorkey=None):
         image = pygame.image.load(f"Sprites/{name}")
@@ -87,7 +91,7 @@ class Player():
             self.rot_image = pygame.transform.rotate(self.image, angle)
 
         rot_image_rect = self.rot_image.get_rect(center = self.rect.center)
-        print(intround(angle, -60, 60))
+        #print(intround(angle, -60, 60))
         rot_image_rect.y += int(-0.5 * intround(angle, -60, 60))
         rot_image_rect.x += int(-0.5 * intround(angle, -60, 60))
 
@@ -101,12 +105,19 @@ class Player():
         screen.blit(self.textR, self.text)
 
         screen.blit(self.rot_image, rot_image_rect.topleft)
-
-        if (self.shooting and self.cadr > 5 and (-50 <= angle <= 60) and self.ammo > self.trat) or (self.dakka and self.shooting and self.cadr > 1 and (-50 <= angle <= 60)):
+        
+        if self.dakka and self.t:
+                dk = pg.mixer.Sound(f'Sounds/Dakka/{random.randint(1, 7)}.mp3')
+                dk.play()
+                self.t = False
+        elif not(self.dakka or self.t):
+            self.t = True
+        
+        if (self.dakka  and self.cadr > self.cadrtoshoot // 2 and (-50 <= angle <= 60)) or (self.shooting and self.cadr > self.cadrtoshoot and (-50 <= angle <= 60) and self.ammo > self.trat):
             self.shootFunc(rx, ry, mouse_pos[0], mouse_pos[1], angle)
-        elif self.cadr > 5:
-            self.cadr = 5
-            if self.ammo < self.maxammo and self.cadr >= 5:
+        elif self.cadr > self.cadrtoshoot:
+            self.cadr = self.cadrtoshoot
+            if self.ammo < self.maxammo and self.cadr >= self.cadrtoshoot:
                 self.ammo += self.plusammo
         else:
             self.cadr += 1
@@ -116,18 +127,25 @@ class Player():
 
         if self.fly:
             self.flyFunc()
+
+
+        
             
 
 class Nobz(Player):
 
     def __init__(self, patron):
-        super().__init__(patron, "Nob", 150, 150, 100, 70, 30, 30)
+        super().__init__(patron, "Nob", 150, 150, 100, 70, 20, 20)
+        self.ammo = 20
+        self.maxammo = 20
 
 
 class Flash(Player):
 
     def __init__(self, patron):
-        super().__init__(patron, "Flash", 170, 170, 150, 70, 60, 60)
+        super().__init__(patron, "Flash", 170, 170, 130, 100, 60, 60, legsmove=70)
+        self.plusammo = 0.2
+        self.trat = 2
 
     def shootFunc(self, rx, ry, mouse_pos0, mouse_pos1, angle):
         self.patron(rx, ry, mouse_pos0, mouse_pos1, angle, self.soundShoot1)
@@ -138,6 +156,27 @@ class Flash(Player):
             self.ammo += self.plusammo
 
     
+class Tank(Player):
+
+    def __init__(self, patron):
+        super().__init__(patron, "Tank", 150, 150, 100, 120, 30, 30, legsmove=30)
+        self.ammo = 5
+        self.maxammo = 5
+        self.plusammo = 0.05
+        self.cadrtoshoot = 30
+        self.soundShoot1 = pg.mixer.Sound('Sounds/zap.mp3')
+
+
+class Meh(Player):
+
+    def __init__(self, patron):
+        super().__init__(patron, "Meh", 180, 150, 100, 120, 30, 30, legsmove=50)
+        self.ammo = 12
+        self.maxammo = 12
+        self.plusammo = 0.05
+        self.cadrtoshoot = 20
+        self.soundShoot1 = pg.mixer.Sound('Sounds/blast.mp3')
+
 
 class Snarad(pygame.sprite.Sprite):
     
@@ -161,12 +200,13 @@ class Snarad(pygame.sprite.Sprite):
         #print(x, y)
         self.i = 1
         self.speed = 10
+        sound.set_volume(0.5)
         sound.play()
 
         
     def update(self):
         if self.i == 300:
-            return False
+            return None
         self.rect = self.rect.move((self.x2 - self.x) // self.speed, (self.y2 - self.y) // self.speed)
         #pygame.draw.line(screen, (255, 0, 0), (self.x, self.y), (self.x2, self.y2))
         self.i += 1
@@ -187,14 +227,14 @@ class Zap(Snarad):
 class Blast(Snarad):
 
     def __init__(self, rx, ry, mouse_pos0, mouse_pos1, angle, sound, ):
-        super().__init__(rx, ry, mouse_pos0, mouse_pos1, angle, sound, 20, 20, 100, "blast.png")
-        self.speed = 70
+        super().__init__(rx, ry, mouse_pos0, mouse_pos1, angle, sound, 25, 20, 100, "blast.png")
+        self.speed = 30
 
 class MiniBolt(Snarad):
 
     def __init__(self, rx, ry, mouse_pos0, mouse_pos1, angle, sound):
         super().__init__(rx, ry, mouse_pos0, mouse_pos1, angle, sound, 20, 7, 20, "bolt.png")
-        self.speed = 11
+        self.speed = 8
 
 class Floor(pygame.sprite.Sprite):
     image = load_image("floor.jpg", 1000, 50)
@@ -213,12 +253,14 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("WAAAAAAAAGHHHH!!!")
     all_sprites = pygame.sprite.Group()
-    Nobz = Flash(MiniBolt)
+    Nobz = Meh(Blast)
     floor = Floor()
     all_sprites.add(floor)
     bolter = pg.mixer.Sound('Sounds/boltshoot2.wav')
     fly = pg.mixer.Sound('Sounds/fly.mp3')
     NoDakka = pygame.USEREVENT + 0
+    CanUseDakka = pygame.USEREVENT + 0
+    CanDakka = True
     pygame.font.init()
 
     running = True
@@ -237,10 +279,13 @@ if __name__ == '__main__':
                     Nobz.go = -4
                 elif event.key == pygame.K_SPACE:
                     Nobz.fly = -10
-                elif event.key == pygame.K_e:
+                elif event.key == pygame.K_e and not(Nobz.dakka):
                     Nobz.dakka = True
                     Nobz.trat = 0
                     pygame.time.set_timer(NoDakka, 3000)
+                    
+                    
+
             
             if event.type == NoDakka:
                 Nobz.dakka = False
