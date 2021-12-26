cafa80aa9759dd596d6128d31f421267a4f3ce51
verdenkaa <nikitapahomov97@gmail.com>
2021-12-26 14:06:56 +0300

    AddBunker
    AddBunker and Tyranid Sprites

--- persons.py
+++ persons.py
@@ -24,15 +24,19 @@ class Player():
         #pygame.sprite.Sprite.__init__(self)
         self.shooting = False
         self.go = False
+        self.position = 100, 500
         self.image = load_image(f"/{person}/Body.png", Bx, By)
         self.rect = self.image.get_rect()
-        self.position = (500 / 2, 500 / 2)
+
+        self.rect.center = self.position[0] + 5, self.position[1] + legsmove
+
+        
         self.rect.center = self.position
         self.image2 = load_image(f"/{person}/Legs.png", Lx, Ly)
         self.rect2 = self.image.get_rect()
         self.rect2.center = self.position[0] + 5, self.position[1] + legsmove
         self.rot_image = self.image
-        self.mask = pygame.mask.from_surface(self.image)
+        #self.mask = pygame.mask.from_surface(self.image)
         self.cadr = 0
         self.fly = False
         self.ammo = ammo
@@ -72,13 +76,14 @@ class Player():
         fly.play()
 
     def shootFunc(self, rx, ry, mouse_pos0, mouse_pos1, angle):
-        self.patron(rx, ry, mouse_pos0, mouse_pos1, angle, self.soundShoot1)
+        all_sprites.add(self.patron(rx, ry, mouse_pos0, mouse_pos1, angle, self.soundShoot1))
         self.cadr = 0
         self.ammo -= self.trat
         if self.dakka and self.ammo < 30:
             self.ammo += self.plusammo
 
 
+    
     def update(self):
         screen.blit(self.image2, self.rect2)
         mouse_pos = pygame.mouse.get_pos()
@@ -95,7 +100,7 @@ class Player():
         rot_image_rect.y += int(-0.5 * intround(angle, -60, 60))
         rot_image_rect.x += int(-0.5 * intround(angle, -60, 60))
 
-        if not pygame.sprite.collide_mask(self, floor):
+        if not(pygame.sprite.collide_mask(self, floor)) and not(pygame.sprite.collide_mask(self, bunker)) and not(pygame.sprite.collide_mask(self, bunker2)):
             self.rect = self.rect.move(0, 7)
             self.rect2 = self.rect2.move(0, 7)
 
@@ -108,6 +113,7 @@ class Player():
         
         if self.dakka and self.t:
                 dk = pg.mixer.Sound(f'Sounds/Dakka/{random.randint(1, 7)}.mp3')
+                dk.set_volume(0.6)
                 dk.play()
                 self.t = False
         elif not(self.dakka or self.t):
@@ -122,10 +128,10 @@ class Player():
         else:
             self.cadr += 1
 
-        if self.go:
+        if self.go and not(self.dakka):
             self.goFunc()
 
-        if self.fly:
+        if self.fly and not(self.dakka):
             self.flyFunc()
 
 
@@ -148,8 +154,8 @@ class Flash(Player):
         self.trat = 2
 
     def shootFunc(self, rx, ry, mouse_pos0, mouse_pos1, angle):
-        self.patron(rx, ry, mouse_pos0, mouse_pos1, angle, self.soundShoot1)
-        self.patron(rx, ry + random.randint(-40, 40), mouse_pos0, mouse_pos1 + random.randint(-40, 40), angle, self.soundShoot1)
+        all_sprites.add(self.patron(rx, ry, mouse_pos0, mouse_pos1, angle, self.soundShoot1))
+        all_sprites.add(self.patron(rx, ry + random.randint(-40, 40), mouse_pos0, mouse_pos1 + random.randint(-40, 40), angle, self.soundShoot1))
         self.cadr = 0
         self.ammo -= self.trat
         if self.dakka and self.ammo < 60:
@@ -200,13 +206,13 @@ class Snarad(pygame.sprite.Sprite):
         #print(x, y)
         self.i = 1
         self.speed = 10
-        sound.set_volume(0.5)
+        sound.set_volume(0.2)
         sound.play()
 
         
     def update(self):
-        if self.i == 300:
-            return None
+        if self.i > 100:
+            self.kill()
         self.rect = self.rect.move((self.x2 - self.x) // self.speed, (self.y2 - self.y) // self.speed)
         #pygame.draw.line(screen, (255, 0, 0), (self.x, self.y), (self.x2, self.y2))
         self.i += 1
@@ -237,7 +243,7 @@ class MiniBolt(Snarad):
         self.speed = 8
 
 class Floor(pygame.sprite.Sprite):
-    image = load_image("floor.jpg", 1000, 50)
+    image = load_image("floor.png", 1280, 50)
 
     def __init__(self):
         super().__init__(all_sprites)
@@ -246,17 +252,47 @@ class Floor(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.bottom = height
 
+
+class Bunker(pygame.sprite.Sprite):
+    image = load_image("bunker.png", 300, 300)
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Bunker.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = height - 40
+        self.rect.x += 30
+
+class Bunker2(pygame.sprite.Sprite):
+    image = load_image("bunker.png", 300, 300)
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Bunker2.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = height - 340
+        self.rect.x += 30
+
 if __name__ == '__main__':
     pygame.init()
+    image = pygame.image.load("Sprites/fon2.png")
     clock = pygame.time.Clock()
-    size = width, height = 1000, 700
-    screen = pygame.display.set_mode(size)
+    size = width, height = 1280, 720
+    image = pygame.transform.scale(image, size)
+    screen = pygame.display.set_mode(size, pg.FULLSCREEN)
     pygame.display.set_caption("WAAAAAAAAGHHHH!!!")
     all_sprites = pygame.sprite.Group()
+
+
     Nobz = Meh(Blast)
+
+
     floor = Floor()
-    all_sprites.add(floor)
-    bolter = pg.mixer.Sound('Sounds/boltshoot2.wav')
+    bunker = Bunker()
+    bunker2 = Bunker2()
+    all_sprites.add(floor, bunker, bunker2)
     fly = pg.mixer.Sound('Sounds/fly.mp3')
     NoDakka = pygame.USEREVENT + 0
     CanUseDakka = pygame.USEREVENT + 0
@@ -265,6 +301,7 @@ if __name__ == '__main__':
 
     running = True
     while running:
+        #print(clock.get_fps())
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -274,11 +311,11 @@ if __name__ == '__main__':
                 Nobz.shooting = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
-                    Nobz.go = 4
+                    Nobz.go = 6
                 elif event.key == pygame.K_a:
-                    Nobz.go = -4
+                    Nobz.go = -6
                 elif event.key == pygame.K_SPACE:
-                    Nobz.fly = -10
+                    Nobz.fly = -15
                 elif event.key == pygame.K_e and not(Nobz.dakka):
                     Nobz.dakka = True
                     Nobz.trat = 0
@@ -299,7 +336,7 @@ if __name__ == '__main__':
                 elif event.key == pygame.K_SPACE:
                     Nobz.fly = False
 
-        screen.fill((0, 0, 0))
+        screen.blit(image, (0, 0))
         all_sprites.update()
         all_sprites.draw(screen)
         Nobz.update()
