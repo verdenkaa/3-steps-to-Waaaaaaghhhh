03d07f4322f40155e9576739a2e7ad39452e3bda
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2022-01-19 17:28:27 +0300

    fix_pep8

--- campaign.py
+++ campaign.py
@@ -8,21 +8,21 @@ from random import randint
 class Menu_sprite(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        #инициализируем спрайты
+        # инициализируем спрайты
         self.image = pygame.Surface((640, 360))
         self.image = load_image('campaign_map.jpg', 1280, 720)
-        #загружаем фон с помощью функции load_image
+        # загружаем фон с помощью функции load_image
         self.rect = self.image.get_rect()
-        #определяем размеры
+        # определяем размеры
         self.rect.center = (640, 360)
-        #указываем центр изображения
+        # указываем центр изображения
 
 
 def load_image(name, h, w, colorkey=None):
     image = pygame.image.load(f"Sprites/{name}")
-    #загружаем изображение из папки спрайт
+    # загружаем изображение из папки спрайт
     image = pygame.transform.scale(image, (h, w))
-    #масштабируем
+    # масштабируем
     return image
 
 
@@ -30,7 +30,7 @@ def load_ork(name, colorkey=None):
     fullname = 'Sprites/' + name
 
     image = pygame.image.load(fullname)
-    #загружаем изображение
+    # загружаем изображение
     if colorkey is not None:
         image = image.convert()
     if colorkey == -1:
@@ -38,168 +38,167 @@ def load_ork(name, colorkey=None):
         image.set_colorkey(colorkey)
     else:
         image = image.convert_alpha()
-    #проверка на colorkey
+    # проверка на colorkey
     return image
 
 
 def but_cl():
     button_sound = pg.mixer.Sound('Sounds/button-pressing.mp3')
-    #загружаем звук нажатия на кнопку
+    # загружаем звук нажатия на кнопку
     button_sound.play()
-    #проигрываем звук
+    # проигрываем звук
 
 
 def this_mission():
     missia = open('Text/mission_number.txt', 'r')
-    #открываем файл с номером миссии
+    # открываем файл с номером миссии
     num_company = int(missia.readlines()[0])
-    #записываем номер компании в num_company
+    # записываем номер компании в num_company
     missia.close()
-    #закрываем файл
+    # закрываем файл
     return num_company
-    #возвращаем номер компании
+    # возвращаем номер компании
 
 
 def dr_pole():
     all_sprites = pygame.sprite.Group()
-    #делаем группу спрайтов
+    # делаем группу спрайтов
     menu = Menu_sprite()
-    #создаём фон
+    # создаём фон
     all_sprites.add(menu)
-    #добавляем фон в группу спрайтов
+    # добавляем фон в группу спрайтов
     all_sprites.draw(screen)
-    #отрисовываем спрайты на экране
+    # отрисовываем спрайты на экране
 
     ork_base = load_image('ork_face.png', 40, 40)
-    #закружаем точку миссии в виде лица орка
+    # закружаем точку миссии в виде лица орка
     ork_base_img = pygame.transform.scale(ork_base, (40, 40))
-    #масштабируем точку
+    # масштабируем точку
     screen.blit(ork_base_img, (coord[0], coord[1]))
-    #отрисовка точки миссии
+    # отрисовка точки миссии
     pygame.display.update()
-    #обновляем экран для отображения элементов
+    # обновляем экран для отображения элементов
 
 
 def monolog(missia_n):
     ork = load_ork(f'{orks[num_ork]}/Body.png')
-    #загружаем вашего орка
+    # загружаем вашего орка
     ork = pygame.transform.scale(ork, (130, 200))
-    #масштабируем его
+    # масштабируем его
     screen.blit(ork, (0, 390))
-    #отрисовываем его
+    # отрисовываем его
 
     fraze = open('Text/suget.txt', 'r', encoding='utf-8')
-    #читаем сюжетные фразы
+    # читаем сюжетные фразы
     ork_dialog = fraze.readlines()[this_mission()][:-1]
-    #убираем '\n'
+    # убираем '\n'
     fraze.close()
     font_comp = pygame.font.Font(None, 50)
-    #выбираем шрифт
+    # выбираем шрифт
     text_comp = font_comp.render(ork_dialog, True, (255, 0, 0))
-    #делаем текст с данным шрифтом
+    # делаем текст с данным шрифтом
     text_comp_w = text_comp.get_width()
-    #получаем его длину
+    # получаем его длину
     kol_str = text_comp_w // 700
-    #смотрим сколько строк перенесётся
+    # смотрим сколько строк перенесётся
     for j in range(kol_str):
-        #запускаем цикл для отрисовки каждого из перенесённых текстов
+        # запускаем цикл для отрисовки каждого из перенесённых текстов
         font_comp = pygame.font.Font(None, 50)
-        #выбираем шрифт
-        text_comp = font_comp.render(ork_dialog[(len(
-            ork_dialog) // kol_str) * j:(len(ork_dialog) // kol_str) * (j + 1)], True, (255, 0, 0))
-        #делаем текст с созданным ранее шрифтом, разделяя его
+        # выбираем шрифт
+        text_del = len(ork_dialog) // kol_str
+        razdel_text = ork_dialog[text_del * j:text_del * (j + 1)]
+        text_comp = font_comp.render(razdel_text, True, (255, 0, 0))
+        # делаем текст с созданным ранее шрифтом, разделяя его
         text_comp_w = text_comp.get_width()
-        #получаем длину текста
+        # получаем длину текста
         screen.blit(text_comp, (150, 400 + (j * 34)))
-        #отрисовываем его
+        # отрисовываем его
     if text_comp_w % 700 != 0:
-        #проверка на наличие оставшихся строк
+        # проверка на наличие оставшихся строк
         font_comp = pygame.font.Font(None, 50)
-        #выбираем шрифт
-        text_comp = font_comp.render(
-            ork_dialog[(len(ork_dialog) // kol_str) * kol_str:], True, (255, 0, 0))
-        #делаем текст с созданным ранее шрифтом, разделяя его
+        # выбираем шрифт
+        text_del = len(ork_dialog) // kol_str
+        razd_text = ork_dialog[text_del * kol_str:]
+        text_comp = font_comp.render(razd_text, True, (255, 0, 0))
+        # делаем текст с созданным ранее шрифтом, разделяя его
         text_comp_w = text_comp.get_width()
-        #получаем длину текста
+        # получаем длину текста
         screen.blit(text_comp, (150, 400 + (kol_str * 34)))
-        #отрисовываем его
+        # отрисовываем его
     if missia_n == 3:
         pygame.draw.rect(screen, (255, 0, 0),
                          (140, 390, 920, 68 + (kol_str * 34)), 1)
-        #отрисовываем окно диалога
+        # отрисовываем окно диалога
     else:
         pygame.draw.rect(screen, (255, 0, 0),
                          (140, 390, 820, 68 + (kol_str * 34)), 1)
-        
-        #отрисовываем окно диалога
+        # отрисовываем окно диалога
     pygame.display.update()
-    #обновляем экран
+    # обновляем экран
 
 
 def draw_game():
     running = True
-    #переменная по которой будет осуществлятся проверка на незавершённость игры
     num_company = this_mission()
-    #проверяем номер миссии
+    # проверяем номер миссии
     monolog(num_company)
-    #отрисовываем диалог
+    # отрисовываем диалог
     if num_company == 3:
-        #проверка на прохождение всех миссий
+        # проверка на прохождение всех миссий
         pg.mixer.music.pause()
-        #останавливаем музыку
+        # останавливаем музыку
         missia = open('Text/mission_number.txt', 'w')
-        #открываем файл
+        # открываем файл
         missia.write('0')
-        #обнуляем количество пройденных миссий, т.к. сюжет пройден
+        # обнуляем количество пройденных миссий, т.к. сюжет пройден
         missia.close()
-        #закрываем файл
+        # закрываем файл
         pygame.time.wait(5000)
-        #даём время игроку для прочтения
+        # даём время игроку для прочтения
         os.startfile('fly')
-        #запускаем ехе с самолётом
+        # запускаем ехе с самолётом
         sys.exit()
-        #закрываем кампанию
+        # закрываем кампанию
     dialog = True
-    #включаем режим диалога
+    # включаем режим диалога
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 os.startfile('menu')
-                #при выходе из игры открываем меню
+                # при выходе из игры открываем меню
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONUP:
                 but_cl()
-                #воспроизводим звук нажатия мыши
+                # воспроизводим звук нажатия мыши
                 if event.button == 1:
-                    #проверка на нажатие левой кнопки мыши
+                    # проверка на нажатие левой кнопки мыши
                     if dialog:
-                        #проверка на режим диалога
+                        # проверка на режим диалога
                         dr_pole()
-                        #перерисовываем поле для удаления диалога
+                        # перерисовываем поле для удаления диалога
                         dialog = False
-                        #убираем режим диалога
-                if not dialog:
-                    if pygame.mouse.get_pos()[0] > coord[0] and pygame.mouse.get_pos()[
-                            0] < (coord[0] + 40):
-                            #проверка на координаты точки по х
-                        if pygame.mouse.get_pos()[1] > coord[1] and pygame.mouse.get_pos()[
-                                1] < (coord[1] + 40):
-                                #проверка на координаты точки по у
-                            game = open('Text/game_regim.txt', 'w')
-                            #в файле режима игры записываем режим кампании
-                            game.write('Campaign')
-                            game.close()
-                            pg.mixer.music.pause()
-                            #ставим музыку на паузу
-                            os.startfile('persons')
-                            #запускаем игру
-                            sys.exit()
-                            #выключаем окно
+                        # убираем режим диалога
+                if pygame.mouse.get_pos()[0] > coord[0]:
+                    if pygame.mouse.get_pos()[0] < (coord[0] + 40):
+                        # проверка на координаты точки по х
+                        if pygame.mouse.get_pos()[1] > coord[1]:
+                            if pygame.mouse.get_pos()[1] < (coord[1] + 40):
+                                # проверка на координаты точки по у
+                                game = open('Text/game_regim.txt', 'w')
+                                # в файле режима игры записываем режим кампании
+                                game.write('Campaign')
+                                game.close()
+                                pg.mixer.music.pause()
+                                # ставим музыку на паузу
+                                os.startfile('persons')
+                                # запускаем игру
+                                sys.exit()
+                                # выключаем окно
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    #при нажатии на escape окно закрывается
+                    # при нажатии на escape окно закрывается
                     running = False
                     os.startfile('menu')
                     sys.exit()
@@ -207,37 +206,37 @@ def draw_game():
 
 if __name__ == '__main__':
     pygame.init()
-    #инициализируем pygame
+    # инициализируем pygame
     size = 1280, 720
-    #устанавливаем размер окна
+    # устанавливаем размер окна
     screen = pygame.display.set_mode(size)
-    #меняем размер окна
+    # меняем размер окна
     pygame.display.set_caption('3-step to Waagh!')
-    #меняем имя окна
+    # меняем имя окна
 
     orks = ['Nob', 'Flash', 'Tank', 'Meh']
-    #список орков
+    # список орков
 
     ork_type = open('Text/player_ork.txt', 'r')
-    #узнаем орка, которого выбрал игрок
+    # узнаем орка, которого выбрал игрок
     num_ork = orks.index(ork_type.readlines()[0])
     ork_type.close()
 
     coord = [randint(900, 1050), randint(250, 400)]
-    #создаём координаты для точки
+    # создаём координаты для точки
 
     dr_pole()
-    #рисуем поле
+    # рисуем поле
 
     pg.mixer.music.load('Sounds/doom_02. Rip & Tear.mp3')
-    #проигрываем фоновую музыку
+    # проигрываем фоновую музыку
     pg.mixer.music.play(-1)
-    #проигрываем музыку с параметром -1 для постоянного проигрывания
+    # проигрываем музыку с параметром -1 для постоянного проигрывания
     pg.mixer.music.set_volume(0.5)
-    #меняем громкость музыки до комфортного
+    # меняем громкость музыки до комфортного
 
     fps = 120
-    #максимально кол-во фпс
+    # максимально кол-во фпс
     clock = pygame.time.Clock()
     clock.tick(fps)
 
--- choice.py
+++ choice.py
@@ -7,35 +7,35 @@ import sys
 class Сhoice_sprite(pygame.sprite.Sprite):
     def __init__(self, name, image_s_x, image_s_y, center_x, center_y):
         pygame.sprite.Sprite.__init__(self)
-        #инициализируем спрайты
+        # инициализируем спрайты
         self.image = pygame.Surface((center_x, center_y))
         self.image = load_image2(name, image_s_x, image_s_y)
-        #загружаем фон с помощью функции load_image
+        # загружаем фон с помощью функции load_image
         self.rect = self.image.get_rect()
-        #определяем размеры
+        # определяем размеры
         self.rect.center = (center_x, center_y)
-        #указываем центр изображения
+        # указываем центр изображения
 
 
 def draw_cursor(screen, posit):
     screen.blit(cursor_image, posit)
-    #рисуем курсо на экране
+    # рисуем курсо на экране
 
 
 def load_image2(name, h, w, colorkey=None):
     image = pygame.image.load(f"Sprites/{name}")
-    #загружаем изображение
+    # загружаем изображение
     image = pygame.transform.scale(image, (h, w))
-    #масштабируем
+    # масштабируем
     return image
-    #возвращаем
+    # возвращаем
 
 
 def load_ork(name, colorkey=None):
     fullname = 'Sprites/' + name
 
     image = pygame.image.load(fullname)
-    #загружаем изображение орка из папки со спрайтами
+    # загружаем изображение орка из папки со спрайтами
     if colorkey is not None:
         image = image.convert()
     if colorkey == -1:
@@ -44,162 +44,162 @@ def load_ork(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
-    #возвращаем изображение
+    # возвращаем изображение
 
 
 def but_cl():
     button_sound = pg.mixer.Sound('Sounds/button-pressing.mp3')
-    #загружаем звук нажатия
+    # загружаем звук нажатия
     button_sound.play()
-    #проигрываем звук
+    # проигрываем звук
 
 
 def start(num_ork):
     global orks
-    #делаем переменную орк глобальной
+    # делаем переменную орк глобальной
 
     all_sprites = pygame.sprite.Group()
-    #создаём группу спрайтов
-    
+    # создаём группу спрайтов
     menu = Сhoice_sprite('fon.jpg', 500, 500, 250, 250)
-    #загружаем фон
+    # загружаем фон
     all_sprites.add(menu)
-    #добавляем спрайт меню к группе спрайтов
+    # добавляем спрайт меню к группе спрайтов
     all_sprites.draw(screen)
-    #отрисовываем спрайты на экран
+    # отрисовываем спрайты на экран
 
     font_comp = pygame.font.Font(None, 50)
-    #делаем шрифт
+    # делаем шрифт
     text_comp = font_comp.render("Ваш орк: ('|_|')", True, (255, 0, 0))
-    #делаем текст для панели информации
+    # делаем текст для панели информации
     text_comp_w = text_comp.get_width()
-    #получаем ширину текста для панели информации
+    # получаем ширину текста для панели информации
     text_comp_h = text_comp.get_height()
-    #получаем высоту текста для панели информации
+    # получаем высоту текста для панели информации
     screen.blit(text_comp, (0, 10))
-    #отрисовываем текст
-    pygame.draw.rect(screen, (255, 0, 0), (0, 10 - 10, 260, text_comp_h + 20), 1)
-    #рисуем окантовку
-    
+    # отрисовываем текст
+    rect_panel = (0, 10 - 10, 260, text_comp_h + 20)
+    pygame.draw.rect(screen, (255, 0, 0), rect_panel, 1)
+    # рисуем окантовку
+
     text_comp = font_comp.render("Следующий", True, (255, 0, 0))
-    #делаем текст для кнопки
+    # делаем текст для кнопки
     text_comp_w = text_comp.get_width()
-    #получаем ширину текста для кнопки
+    # получаем ширину текста для кнопки
     text_comp_h = text_comp.get_height()
-    #получаем высоту текста для кнопки
+    # получаем высоту текста для кнопки
     screen.blit(text_comp, (270, 90))
-    #отрисовываем текст
-    pygame.draw.rect(screen, (0, 0, 0), (270 - 10, 90 - 10,
-                                         text_comp_w + 20, text_comp_h + 20), 2)
-    #рисуем окантовку кнопки
-    
+    # отрисовываем текст
+    rect_button = (270 - 10, 90 - 10,  text_comp_w + 20, text_comp_h + 20)
+    pygame.draw.rect(screen, (0, 0, 0), rect_button, 2)
+    # рисуем окантовку кнопки
+
     text_comp = font_comp.render("Прошлый", True, (255, 0, 0))
-    #делаем текст для кнопки
+    # делаем текст для кнопки
     text_comp_w = text_comp.get_width()
-    #получаем ширину текста для кнопки
+    # получаем ширину текста для кнопки
     text_comp_h = text_comp.get_height()
-    #получаем высоту текста для кнопки
+    # получаем высоту текста для кнопки
     screen.blit(text_comp, (300, 180))
-    #отрисовываем текст
-    pygame.draw.rect(screen, (0, 0, 0), (270 - 10, 180 - 10,
-                                         text_comp_w + 70, text_comp_h + 20), 2)
-    #рисуем окантовку кнопки
+    # отрисовываем текст
+    rect_button = (270 - 10, 180 - 10, text_comp_w + 70, text_comp_h + 20)
+    pygame.draw.rect(screen, (0, 0, 0), rect_button, 2)
+    # рисуем окантовку кнопки
 
     font_comp = pygame.font.Font(None, 39)
     text_comp = font_comp.render(
         "Немного информации о вашем орке:", True, (255, 0, 0))
-    #делаем текст для панели информации
+    # делаем текст для панели информации
     text_comp_w = text_comp.get_width()
-    #получаем ширину текста для панели информации
+    # получаем ширину текста для панели информации
     text_comp_h = text_comp.get_height()
-    #получаем высоту текста для панели информации
+    # получаем высоту текста для панели информации
     screen.blit(text_comp, (0, 300))
-    #отрисовываем текст
-    pygame.draw.rect(screen, (255, 0, 0), (0, 300 - 10,
-                                           488, text_comp_h + 20), 1)
-    #рисуем окантовку панели информации
+    # отрисовываем текст
+    rect_panel = (0, 300 - 10, 488, text_comp_h + 20)
+    pygame.draw.rect(screen, (255, 0, 0), rect_panel, 1)
+    # рисуем окантовку панели информации
 
     text = open('Text/orks_info.txt', 'r', encoding='utf-8')
-    #открываем файл с информацией об орках
+    # открываем файл с информацией об орках
     ork_info = text.readlines()[num_ork].split('\n')[0]
-    #записываем информацию об орке в переменную
+    # записываем информацию об орке в переменную
     text.close()
-    #закрываем файл
+    # закрываем файл
     font_comp = pygame.font.Font(None, 20)
-    #устанавливаем шрифт
+    # устанавливаем шрифт
     text_comp = font_comp.render(ork_info, True, (255, 0, 0))
-    #создаем текст
+    # создаем текст
     text_comp_w = text_comp.get_width()
-    #получаем ширину текста
+    # получаем ширину текста
     kol_str = text_comp_w // 500
-    #получаем количество разделённых строк
+    # получаем количество разделённых строк
     for i in range(kol_str):
         font_comp = pygame.font.Font(None, 17)
-        #устанавливаем шрифт
-        text_comp = font_comp.render(ork_info[(len(
-            ork_info) // kol_str) * i:(len(ork_info) // kol_str) * (i + 1)], True, (255, 0, 0))
-        #создаем текст
+        # устанавливаем шрифт
+        text_comp = font_comp.render(ork_info[(len(ork_info) // kol_str) * i:(
+            len(ork_info) // kol_str) * (i + 1)], True, (255, 0, 0))
+        # создаем текст
         screen.blit(text_comp, (0, 370 + (i * 15)))
-        #отрисовываем текст
+        # отрисовываем текст
     if text_comp_w % 500 != 0:
         font_comp = pygame.font.Font(None, 17)
-        #устанавливаем шрифт
+        # устанавливаем шрифт
         text_comp = font_comp.render(
             ork_info[(len(ork_info) // kol_str) * kol_str:], True, (255, 0, 0))
-        #создаем текст
+        # создаем текст
         screen.blit(text_comp, (0, 370 + (kol_str * 15)))
-        #отрисовываем текст
+        # отрисовываем текст
 
     ork = load_ork(f'{orks[num_ork]}/Body.png')
-    #запускаем функцию для отрисовки орка
+    # запускаем функцию для отрисовки орка
     play_ork = open('Text/player_ork.txt', 'w')
-    #открываем файл для записи имени орка
+    # открываем файл для записи имени орка
     play_ork.write(orks[num_ork])
-    #записываем имя другого орка
+    # записываем имя другого орка
     play_ork.close()
-    #закрываем файл
+    # закрываем файл
     ork = pygame.transform.scale(ork, (130, 200))
-    #масштабируем орка
+    # масштабируем орка
     screen.blit(ork, (70, 80))
-    #отрисовываем его
+    # отрисовываем его
     pygame.display.update()
-    #обновляем экран
+    # обновляем экран
 
 
 if __name__ == '__main__':
     pygame.init()
-    #инициализируем pygame
+    # инициализируем pygame
     size = width, height = 500, 500
-    #устанавливаем размер окна
+    # устанавливаем размер окна
     screen = pygame.display.set_mode(size)
-    #меняем размер окна
+    # меняем размер окна
     pygame.display.set_caption('3-step to Waagh! Choose your Orks!')
-    #меняем оглавление окна
+    # меняем оглавление окна
 
     cursor_image = load_image2('cursor.png', 25, 25)
-    #загружаем курсор
+    # загружаем курсор
     pygame.mouse.set_visible(False)
-    #отключаем видимость системного курсора
+    # отключаем видимость системного курсора
 
     orks = ['Nob', 'Flash', 'Tank', 'Meh']
-    #список орков
+    # список орков
 
     pg.mixer.music.load('Sounds/doom_02. Rip & Tear.mp3')
-    #загружаем фоновую музыку
+    # загружаем фоновую музыку
     pg.mixer.music.play(-1)
-    #запускаем ее в бескнонечном проигрывании
+    # запускаем ее в бескнонечном проигрывании
     pg.mixer.music.set_volume(0.25)
-    #меняем громкость до комфортной
+    # меняем громкость до комфортной
 
     ork_type = open('Text/player_ork.txt', 'r')
-    #читаем орка, которого игрок выбрал до этого
+    # читаем орка, которого игрок выбрал до этого
     start(orks.index(ork_type.readlines()[0]))
-    #отрисовываем интерфейс
+    # отрисовываем интерфейс
     ork_type.close()
-    #закрываем файл
+    # закрываем файл
 
     fps = 120
-    #указываем максимальны фпс
+    # указываем максимальны фпс
     clock = pygame.time.Clock()
     clock.tick(fps)
 
@@ -207,65 +207,68 @@ if __name__ == '__main__':
 
     while pygame.event.wait().type != pygame.QUIT:
         ork_type = open('Text/player_ork.txt', 'r')
-		#читаем орка, которого игрок выбрал до этого
+        # читаем орка, которого игрок выбрал до этого
         num_ork = orks.index(ork_type.readlines()[0])
-        #узнаем его номер в списке
+        # узнаем его номер в списке
         ork_type.close()
-        #закрываем файл
+        # закрываем файл
 
         running = True
 
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-					#при выходе из игры возвращаем игрока в меню
+                    # при выходе из игры возвращаем игрока в меню
                     running = False
                     os.startfile('menu')
                     sys.exit()
-                    #закрываем окно
+                    # закрываем окно
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
-						#на клавишу escape закрываем окно и включаем меню
+                        # на клавишу escape закрываем окно и включаем меню
                         running = False
                         os.startfile('menu')
                         sys.exit()
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    if pygame.mouse.get_pos()[0] > 260 and pygame.mouse.get_pos()[
-                            0] < 498:
-								#проверка на координаты точки по х
-                        if pygame.mouse.get_pos()[1] > 80 and pygame.mouse.get_pos()[
-                                1] < 135:
-									#проверка на координаты точки по у
-                            but_cl()
-                            #воспроизводим звук нажатия на мышь
-                            num_ork += 1
-                            #увеличиваем номер орка, т.к. в этих координатах находится кнопка следующий
-                            if num_ork > len(orks) - 1:
-								#если номер орка больше чем максимальный индекс списка
-                                num_ork = 0
-                                #устанавливаем номер орка = 0
-                            start(num_ork)
-                            #перерисовываем экран
-                    if pygame.mouse.get_pos()[0] > 260 and pygame.mouse.get_pos()[
-                            0] < 498:
-								#проверка на координаты точки по х
-                        if pygame.mouse.get_pos()[1] > 170 and pygame.mouse.get_pos()[
-                                1] < 225:
-									#проверка на координаты точки по у
-                            but_cl()
-                            #воспроизводим звук нажатия на мышь
-                            num_ork -= 1
-                            #уменьшаем номер орка, т.к. в этих координатах находится кнопка предыдущий
-                            if num_ork < 0:
-								#если номер орка меньше чем 0
-                                num_ork = 3
-                                #устанавливаем номер орка = 3
-                            start(num_ork)
-                            #перерисовываем экран
+                    if pygame.mouse.get_pos()[0] > 260:
+                        if pygame.mouse.get_pos()[0] < 498:
+                                # проверка на координаты точки по х
+                            if pygame.mouse.get_pos()[1] > 80:
+                                if pygame.mouse.get_pos()[1] < 135:
+                                        # проверка на координаты точки по у
+                                    but_cl()
+                                    # воспроизводим звук нажатия на мышь
+                                    num_ork += 1
+                                    # увеличиваем номер орка, т.к. в этих
+                                    # координатах находится кнопка следующий
+                                    if num_ork > len(orks) - 1:
+                                        # если номер орка больше
+                                        # чем максимальный индекс списка
+                                        num_ork = 0
+                                        # устанавливаем номер орка = 0
+                                    start(num_ork)
+                                    # перерисовываем экран
+                    if pygame.mouse.get_pos()[0] > 260:
+                        if pygame.mouse.get_pos()[0] < 498:
+                                # проверка на координаты точки по х
+                            if pygame.mouse.get_pos()[1] > 170:
+                                if pygame.mouse.get_pos()[1] < 225:
+                                        # проверка на координаты точки по у
+                                    but_cl()
+                                    # воспроизводим звук нажатия на мышь
+                                    num_ork -= 1
+                                    # уменьшаем номер орка, т.к. в этих
+                                    # координатах находится кнопка предыдущий
+                                    if num_ork < 0:
+                                        # если номер орка меньше чем 0
+                                        num_ork = 3
+                                        # устанавливаем номер орка = 3
+                                    start(num_ork)
+                                    # перерисовываем экран
             start(num_ork)
-            #обновляем экран
+            # обновляем экран
             draw_cursor(screen, pygame.mouse.get_pos())
-            #рисуем наш курсор
+            # рисуем наш курсор
             pygame.display.flip()
 
     pygame.quit()
